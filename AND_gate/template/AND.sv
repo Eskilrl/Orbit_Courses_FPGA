15e7e003d6cb8fdf1396ede05cce291c7d574425
Eskilrl <eskilrl@yahoo.com>
2024-09-19 15:16:43 +0200

    Added new constraint file, uses iceStorm now instead of diamond

--- AND_gate/template/AND.sv
+++ AND_gate/template/AND.sv
@@ -1,14 +1,12 @@
 module AND(
     //place inputs and outputs here:
+    input i_SW2, // SW2
+    input i_SW4, // SW4
 
-    //Example:
-
-    //input a,
-    //input b,
-
-    //output c
+    output o_Led1 // D1
 );
 
+assign o_Led1 = i_SW2 & i_SW4;
 
 //Write your logic here:
 
